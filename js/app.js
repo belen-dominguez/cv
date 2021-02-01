e51897955e3b829b0e8313ffb44eb8ee883041bf
belen-dominguez <belen.dominguez.sigler@outlook.com>
2021-02-01 16:20:37 -0300

    my work

--- js/app.js
+++ js/app.js
@@ -12,6 +12,81 @@ window.addEventListener('scroll', () => {
 
  window.addEventListener('load', titleEffect)
 
+/*Education */ 
+const educationDiv = document.querySelector('.education-container')
+const educationDivChildren = document.querySelector('.education-container').children
+
+
+console.log(educationDiv.offsetTop)
+console.log(educationDiv.getBoundingClientRect().top)
+
+
+console.log(educationDivChildren[0].children[0])
+
+window.addEventListener('scroll', () => {
+    //console.log(educationDiv.getBoundingClientRect().top)
+    if(educationDiv.getBoundingClientRect().top > 170 && educationDiv.getBoundingClientRect().top < 280 ){
+
+        educationDiv.addEventListener('scroll', () => {
+            console.log('parent', educationDivChildren[0].getBoundingClientRect().top)
+            console.log('child', educationDivChildren[0].children[0].getBoundingClientRect().top)
+
+        })
+
+        /*
+
+        hijo esta en pantalla desde 
+        parent -> 143
+        child -> 159
+        hasta
+        parent -> -54
+        child -> -38
+
+
+
+        arriba de todo
+        parent -> 143
+        child -> 159
+-> 16 
+
+        scrolleando hacia abajo. a la mitad del  hijo
+        parent -> 33
+        child -> 49
+        scrolleando hacia abajoi. cuando desaparece el hijo
+        parent -> -39
+        child -> -23
+
+
+        scrolleando hacia arriba. cuando se empieza a ver el hijo
+        parent -> 7
+        child -> 23
+        scrolleando hacia arriba. cuando se completa el hijo
+        parent -> 138
+        child -> 154
+
+
+
+        medio
+         parent -> -723
+        child -> -707
+
+        abajo de todo
+        parent -> -1788
+        child _> -1772
+
+
+
+        */
+      
+        // educationDivChildren[0].addEventListener('scroll', () => {
+
+        // })
+    }
+
+})
+
+
+
 
  /*Display work*/
  
